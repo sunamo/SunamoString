d56dcc7ef2d88e978910c3982962895f82a2dda4
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-04 12:09:14 +0100

    .

--- _sunamo/SHSplit.cs
+++ _sunamo/SHSplit.cs
@@ -6,37 +6,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SunamoString._sunamo
+namespace SunamoString._sunamo;
+
+internal class SHSplit
 {
-    internal class SHSplit
+    internal static void SplitByIndex(string p, int firstNormal, out string title, out string remix)
     {
-        internal static void SplitByIndex(string p, int firstNormal, out string title, out string remix)
-        {
-            title = p.Substring(0, firstNormal);
-            remix = p.Substring(firstNormal + 1);
-        }
-        internal static List<string> Split(string p, params string[] newLine)
-        {
-            return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
-        }
+        title = p.Substring(0, firstNormal);
+        remix = p.Substring(firstNormal + 1);
+    }
+    internal static List<string> Split(string p, params string[] newLine)
+    {
+        return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
 
-        internal static List<string> SplitNoneChar(string p, params char[] newLine)
-        {
-            return p.Split(newLine, StringSplitOptions.None).ToList();
-        }
-        internal static List<string> SplitNone(string p, params string[] newLine)
-        {
-            return p.Split(newLine, StringSplitOptions.None).ToList();
-        }
+    internal static List<string> SplitNoneChar(string p, params char[] newLine)
+    {
+        return p.Split(newLine, StringSplitOptions.None).ToList();
+    }
+    internal static List<string> SplitNone(string p, params string[] newLine)
+    {
+        return p.Split(newLine, StringSplitOptions.None).ToList();
+    }
 
-        internal static List<string> SplitChar(string p, params char[] newLine)
-        {
-            return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
-        }
+    internal static List<string> SplitChar(string p, params char[] newLine)
+    {
+        return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
 
-        internal static List<string> SplitCharNone(string p, params char[] newLine)
-        {
-            return p.Split(newLine, StringSplitOptions.None).ToList();
-        }
+    internal static List<string> SplitCharNone(string p, params char[] newLine)
+    {
+        return p.Split(newLine, StringSplitOptions.None).ToList();
     }
 }
