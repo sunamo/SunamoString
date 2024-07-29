f99ede03e95241d0fc46f3c7ab17a8c67d6f1193
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-29 15:03:03 +0200

    24.7.29.1 Added comments

--- Enums/SearchStrategy.cs
+++ Enums/SearchStrategy.cs
@@ -8,16 +8,16 @@ namespace SunamoString.Enums;
 
 public enum SearchStrategy
 {
-
-
-
+    /// <summary>
+    /// Contains
+    /// </summary>
     FixedSpace,
-
-
-
+    /// <summary>
+    /// split input by spaces and A1 must contains all parts
+    /// </summary>
     AnySpaces,
-
-
-
+    /// <summary>
+    /// Is exactly the same
+    /// </summary>
     ExactlyName
 }
