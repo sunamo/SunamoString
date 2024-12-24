e674969e35523a986d408c61720e72de210c19cb
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-24 20:52:42 +0100

    Removed unused element from csproj

--- _sunamo/SunamoValues/Values/SunamoNotTranslateAble.cs
+++ _sunamo/SunamoValues/Values/SunamoNotTranslateAble.cs
@@ -4,8 +4,8 @@ namespace SunamoString._sunamo.SunamoValues.Values;
 internal class SunamoNotTranslateAble
 {
     internal const string From = " - From";
-    internal const string SessI18n = "sess.i18n(";
-    internal const string SessI18nShort = "sess.i18n(";
+    internal const string SessI18n = "Translate.FromKey(";
+    internal const string SessI18nShort = "Translate.FromKey(";
     internal const string RLDataEn = "RLData.en[";
     internal const string RLDataCs = "RLData.cs[";
     internal const string RLDataEn2 = "RLDataEn[";
